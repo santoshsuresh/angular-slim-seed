b7fb9e4d70ff899d5866f034e1ebf430bc05339d
Santosh Suresh <santosh.s@telibrahma.com>
2012-10-28 23:12:29 +0530

    Added support for the slim app

--- index.php
+++ index.php
@@ -5,12 +5,26 @@ require_once("./vendor/autoload.php");
 $app = new \Slim\Slim();
 
 $app->get('/','getIndex');
+$app->get('/api/names','getNames');
 
 
 # Functions
 
 function getIndex() {
     echo "Works";
+}
+
+
+function getNames() {
+    $result = array(
+        array('name'=>'Name 1','city'=>'City'),
+        array('name'=>'Name 2','city'=>'City'),
+        array('name'=>'Name 3','city'=>'City'),
+        array('name'=>'Name 4','city'=>'City'),
+        array('name'=>'Name 5','city'=>'City'),
+        array('name'=>'Name 6','city'=>'City'),
+    );
+    echo json_encode($result);
 }
 
 $app->run();
